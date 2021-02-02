0e93ce0d645dea113429e77583c70dd284c1cb5e
Stas Lifanov <holidayonmail@gmail.com>
2021-02-03 00:22:21 +0300

    Header created

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -5,6 +5,8 @@ import Button from "@material-ui/core/Button";
 
 import { Link } from "react-router-dom";
 
+import CityLogo from "../ui/icons";
+
 class Header extends Component {
   render() {
     return (
@@ -19,13 +21,20 @@ class Header extends Component {
       >
         <Toolbar styles={{ display: "flex" }}>
           <div styles={{ flexGrow: 1 }}>
-            <div className={"header_logo"}>LOGO</div>
+            <div className={"header_logo"}>
+              <CityLogo
+                link={true}
+                linkTo={"/"}
+                width={"70px"}
+                height={"70px"}
+              />
+            </div>
           </div>
           <Link to={"/the_team"}>
-            <Button color={"inherit"}></Button>
+            <Button color={"inherit"}>The team</Button>
           </Link>
           <Link to={"/the_matches"}>
-            <Button color={"inherit"}></Button>
+            <Button color={"inherit"}>Matches</Button>
           </Link>
         </Toolbar>
       </AppBar>
